b3001f3d41f3687ba173c053c418b6174094946e
larafazani <laraegreja@gmail.com>
2026-04-08 20:41:48 -0300

    Add files via upload

---
+++ papagaio.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    if (argc == 1)
+    {
+        printf("voce nao digitou nada\n");
+        exit(0);
+    }
+
+    printf("voce digitou %s\n", argv[1]);
+
+    return 0;
+}
